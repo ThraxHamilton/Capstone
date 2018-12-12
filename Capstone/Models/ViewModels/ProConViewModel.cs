0ef2603648252e7492da0ad1802991abe3a4c325
Michael McClenton <mmcclenton16@gmail.com>
2018-12-12 11:25:23 -0600

    good good

---
+++ Capstone/Models/ViewModels/ProConViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models.ViewModels
+{
+    public class ProConViewModel
+    {
+        public Pros Pros { get; set; }
+
+        public Cons Cons { get; set; }
+
+        public string Date { get; set; }
+        public string ProEntry { get; set; }
+        public string ConEntry { get; set; }
+
+
+    }
+}
